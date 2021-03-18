bede676f5492e9e8b4b93f3e684fb1f8340e7772
Victor Huatuco <victorhuatuco@gmail.com>
2021-03-18 17:50:39 -0500

    Boton_Led_RbPi

--- ventana_gpio.py
+++ ventana_gpio.py
@@ -1,5 +1,10 @@
-from  PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
+from  PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 import sys
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.out)
 
 class App(QWidget):
     def __init__(self): #Establece los valores de la ventana
@@ -13,17 +18,26 @@ class App(QWidget):
     def initUI(self): 	#Coloca los valores a QtWidget
         self.setWindowTitle(self.title)	
         self.setGeometry(self.m,self.n,self.ancho,self.alto)
-        #Label
-        label1 = QLabel("Hola mundo",self) #Texto de label
-        label1.move(50,50) #Posicion de label
-        #boton
-        button = QPushButton("Activar", self) #Texto del boton
-        button.move(100, 100) #Posicion del boton
+        
+        #boton encendido
+        button = QPushButton("Encender", self) #Texto del boton
+        button.move(20, 80) #Posicion del boton
         button.clicked.connect(self.on) #conecta a la accion del boton a un metodo
+
+        #boton apagado
+        button2 = QPushButton("Apagar", self) #Texto del boton
+        button2.move(100, 100) #Posicion del boton
+        button2.clicked.connect(self.off) #conecta a la accion del boton a un metodo
+        
         self.show() #Muestra la ventana
+
         
     def on(self): #Accion del boton
-        print("Se presionó el boton")
+        GPIO.output(21,True)
+        print("Led encendido")
+    def off(self): #Accion del boton
+        GPIO.output(21,Flase)
+        print("Led apagado")
 
 if __name__ == "__main__":
     app = QApplication([]) 
